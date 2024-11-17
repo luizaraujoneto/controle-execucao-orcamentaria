30a7fd89009c058449fc6381629e640025102d4d
Luiz Araujo <luizaraujoneto@gmail.com>
2024-11-17 12:34:55 -0300

    Criação da página com informações de Execução Orçamentária Detalhada - WIP

--- execucaoorcamentariadetalhada/views.py
+++ execucaoorcamentariadetalhada/views.py
@@ -9,6 +9,7 @@ def execucaoDetalhada_list(request):
     table.paginate(page=request.GET.get("page", 1), per_page=100)
     return render(request, "execucaoorcamentariadetalhada/execucaodetalhada_list.html", {"table": table})
 
+
 def execucaoDetalhada_detail(request, pk):
     ed = get_object_or_404(ExecucaoDetalhada, id=pk)
 
